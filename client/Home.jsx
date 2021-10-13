7e88d74c6a448bc3688f54d7cdf864365f435896
Iris Huang <yilu.huang@gmail.com>
2021-10-13 12:44:02 -0700

    "Added icons"

--- client/Home.jsx
+++ client/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-
-
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -11,8 +9,21 @@ class Home extends React.Component {
   render() {
     return(
       <React.Fragment>
-        <h1>Rotating Text Around The Earth</h1>
-        <div className="earth">hi</div>
+        {/* <div className="session">
+          <div className="earth"></div>
+            <div className="circle">
+              <span style={{"--i": 1}} key="1">S</span>
+              <span style={{"--i": 2}} key="2">Y</span>
+              <span style={{"--i": 3}} key="3">N</span>
+              <span style={{"--i": 4}} key="4">C</span>
+              <span style={{"--i": 5}} key="5">-</span>
+              <span style={{"--i": 6}} key="6">S</span>
+              <span style={{"--i": 7}} key="7">P</span>
+              <span style={{"--i": 8}} key="8">A</span>
+              <span style={{"--i": 9}} key="9">C</span>
+              <span style={{"--i": 10}} key="10">E</span>
+          </div>
+        </div> */}
       </React.Fragment>
     );
   }
